c27c81d7d10fa814ceb8198d3b32035b81ab4aa8
Greg Leitheiser <greg@servantscode.org>
2020-10-15 13:38:06 -0500

    Seperate Campus Ministry from College

--- quick-start.sh
+++ quick-start.sh
@@ -11,7 +11,8 @@ echo "Configuring service $SERVICE"
 files="Dockerfile kube.yml deploy.env settings.gradle src/main/webapp/WEB-INF/web.xml"
 classes="src/main/java/com/newmanconnection/hello/Hello.java src/main/java/com/newmanconnection/hello/db/HelloDB.java src/main/java/com/newmanconnection/hello/rest/ServiceIdentificationFilter.java src/main/java/com/newmanconnection/hello/rest/ServiceConfiguration.java src/main/java/com/newmanconnection/hello/rest/HelloSvc.java src/main/java/com/newmanconnection/hello/db/DBUpgrade.java"
 
-CAP_SERVICE="$(tr '[:lower:]' '[:upper:]' <<< ${SERVICE:0:1})${SERVICE:1}"
+CAMEL_SERVICE=`echo $SERVICE | sed -r 's/(_|-)([a-z])/\U\2/g'`
+CAP_SERVICE="$(tr '[:lower:]' '[:upper:]' <<< ${CAMEL_SERVICE:0:1})${CAMEL_SERVICE:1}"
 DATE=`date +%D`
 USER=`whoami`
 printf "Service: $CAP_SERVICE\nCreator: $USER\nCreated: $DATE" > README.md
@@ -24,14 +25,14 @@ done
 
 for file in $classes; do 
   echo "Updating $file"
-  sed -i "s/hello/$SERVICE/g" $file
+  sed -i "s/hello/$CAMEL_SERVICE/g" $file
   sed -i "s/Hello/$CAP_SERVICE/g" $file
 done
 
 mv src/main/java/com/newmanconnection/hello/Hello.java src/main/java/com/newmanconnection/hello/$CAP_SERVICE.java
 mv src/main/java/com/newmanconnection/hello/db/HelloDB.java src/main/java/com/newmanconnection/hello/db/${CAP_SERVICE}DB.java
 mv src/main/java/com/newmanconnection/hello/rest/HelloSvc.java src/main/java/com/newmanconnection/hello/rest/${CAP_SERVICE}Svc.java
-mv src/main/java/com/newmanconnection/hello src/main/java/com/newmanconnection/$SERVICE
+mv src/main/java/com/newmanconnection/hello src/main/java/com/newmanconnection/$(tr '[:upper:]' '[:lower:]' $CAMEL_SERVICE)
 
 git rm -f quick-start.sh 
 git remote set-url origin git@github.com:newmanconnection/$SERVICE-svc.git
